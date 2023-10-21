05759ca95e25cbf402d1f5bca22fa12a0053c4e2
Jim <146436495+S275283@users.noreply.github.com>
2023-10-21 08:34:23 +0100

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,9 @@
+# Initialize a variable to store the sum
+total_sum = 0
+
+# Use a for loop to iterate through numbers from 1 to 10
+for number in range(1, 11):
+    total_sum += number  # Add the current number to the total sum
+
+# Print the result
+print("The sum of numbers from 1 to 10 is:", total_sum)
